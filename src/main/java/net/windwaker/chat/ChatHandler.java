031a4c57455b087db373f947feae616858c418e4
Windwaker <wjcrouse913@aim.com>
2012-05-01 20:09:15 -0400

    Initial commit

---
+++ src/main/java/net/windwaker/chat/ChatHandler.java
@@ -0,0 +1,59 @@
+/* Copyright (c) 2012 Walker Crouse, http://windwaker.net/
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package net.windwaker.chat;
+
+import org.spout.api.event.EventHandler;
+import org.spout.api.event.Listener;
+import org.spout.api.event.Order;
+import org.spout.api.event.player.PlayerChatEvent;
+import org.spout.api.player.Player;
+
+/**
+ * Handles formatting of messages from players.
+ * @author Windwaker
+ */
+public class ChatHandler implements Listener {
+	
+	@EventHandler(order = Order.LATEST)
+	public void formatMessage(PlayerChatEvent event) {
+		// Get the configured chat format
+		Player player = event.getPlayer();
+		String format = player.getData("chat-format").getString();
+		if (format == null) {
+			return;
+		}
+		
+		// Replace all the tags and variables.
+		format = format.replaceAll("%name%", "%1$s").replaceAll("%message%", "%2$s");
+		for (String variable : format.split("%")) {
+			String value = player.getData(variable).getString();
+			if (value == null) {
+				continue;
+			}
+			
+			format = format.replaceAll("%" + variable + "%", value);
+		}
+		
+		// Set the format of the message to be sent
+		event.setFormat(format);
+	}
+}
